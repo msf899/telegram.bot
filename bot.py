2103b55940c82d23314df95a06e34d8dc8431dfa
msf899 <shahzodmahmudov14@gmail.com>
2026-05-09 19:41:48 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,7 +24,7 @@ BAD_WORDS = [
     "sikaman",
     "siktim",
     "sikey",
-    "siki",
+    "siki"w,
     "aminga qotogim",
     "popish",
     "xuyela",
@@ -37,6 +37,7 @@ BAD_WORDS = [
     "qoto",
     "qotoq",
     "yiban",
+    "skb",
     "oneni ami",
     "am",
 ]
