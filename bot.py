241ee0fada2e26267a959c04b31040a1e82fb707
makhmudovs <shahzodmahmudov14@gmail.com>
2026-05-18 22:20:39 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -141,7 +141,7 @@ async def anti_mat(update: Update, context: ContextTypes.DEFAULT_TYPE):
             warning = await update.message.chat.send_message(
                 f"⚠️ {name}, iltimos so'kinmang! Guruh qoidalariga rioya qiling. 🙏"
             )
-            context.application.create_task(delete_after(warning, 3 * 60))
+            context.application.create_task(delete_after(warning, 4 * 60))
         except Exception:
             pass
 
